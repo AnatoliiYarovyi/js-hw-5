008e20300505ef867461a41906b61c2fe1be825e
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2020-12-14 03:32:23 +0200

    Добавляет дз_05

--- js/03.js
+++ js/03.js
@@ -1,45 +1,65 @@
-const add = (accum, element) => accum + element;
-const mult = (accum, element) => accum * element;
-const sub = (accum, element) => accum - element;
-
-function reduceArray(array, cb, initial) {
-  'use strict';
-  let i;
-  let accum;
-  if (arguments.length >= 3) {
-    accum = initial;
-    i = 0;
+// Write code under this line
+class Storage {
+  constructor(items) {
+    this.items = items;
   }
-  if (arguments.length === 2) {
-    accum = array[0];
-    i = 1;
+  getItems() {
+    console.log(this.items);
   }
-  for (i; i < array.length; i += 1) {
-    const element = array[i];
-    // Write code under this line
-    accum = cb(accum, element);
+  addItem(item) {
+    this.items.push(item);
+    return this.items;
   }
-  return accum;
+  removeItem(item) {
+    for (let i = this.items.length; i--; ) {
+      if (this.items[i] === item) {
+        return this.items.splice(i, 1);
+      }
+    }
+  }
 }
+console.log(typeof Storage);
+// 'function'
 
-const arr = [1, 2, 3, 4, 5];
+const goods = ['Нанитоиды', 'Пролонгер', 'Железные жупи', 'Антигравитатор'];
 
-console.log(reduceArray(arr, add)); // 15
-console.log(reduceArray(arr, add, 10)); // 25
+const storage = new Storage(goods);
 
-console.log(reduceArray(arr, mult)); // 120
-console.log(reduceArray(arr, mult, 10)); // 1200
+console.log(storage.getItems());
+/* [
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор'
+] */
 
-console.log(reduceArray(arr, sub)); // -13
-console.log(reduceArray(arr, sub, 10)); // -5
+storage.addItem('Дроид');
+console.log(storage.getItems());
+/* [
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор',
+  'Дроид'
+] */
 
-// ----------------------------------------------------------------------------------------------------------
-// ======== Задача 4 - 3 ========
-// ------------------ Callback функция для получения одного вычисляемого значения массива -------------
-// Функции add, sub и mult принимают два параметра - accum и element,
-// возвращает число - сумму, разность или произведение параметров.
+storage.removeItem('Пролонгер');
+console.log(storage.getItems());
+/* [
+  'Нанитоиды',
+  'Железные жупи',
+  'Антигравитатор',
+  'Дроид'
+] */
 
-// Дополни тело функции reduceArray строкой присвоения accum вызова функции cb.
-// Функция reduceArray должна будет подсчитать сумму или разность или
-// произведение всех элементов массива в зависимости от того какая именно
-// из трех функция(add, mult, sub) будет передана в качестве cb.
+// ------------------------------------------------------------------------------------------
+// ===== Задача 5-3 =====
+// ------ использование методов класса -----
+// Напиши класс Storage, который будет создавать объекты для управления складом товаров.
+// При вызове будет получать один аргумент - начальный массив товаров, и записывать его в свойство items.
+
+// Добавь методы класса:
+
+// getItems() - возвращает массив текущих товаров
+// addItem(item) - получает новый товар и добавляет его к текущим
+// removeItem(item) - получает товар и, если он есть, удаляет его из текущих
--- js/05.js
+++ js/05.js
@@ -1,50 +1,203 @@
-const inventory = {
-  items: ['Knife', 'Gas mask'],
-  add(itemName) {
-    this.items.push(itemName);
-    return `Adding ${itemName} to inventory`;
-  },
-  remove(itemName) {
-    this.items = this.items.filter(item => item !== itemName);
-    return `Removing ${itemName} from inventory`;
-  },
-};
+class Car {
+  // Write code under this line
+  static getSpecs(car) {
+    return `maxSpeed: ${car.maxSpeed}, speed: ${car.speed}, isOn: ${car.isOn}, distance: ${car.distance}, price: ${car._price}`;
+  }
+  constructor(car) {
+    this.speed = 0;
+    this._price = car.price;
+    this.maxSpeed = car.maxSpeed;
+    this.isOn = false;
+    this.distance = 0;
+  }
+  get price() {
+    return this._price;
+  }
+  set price(value) {
+    this._price = value;
+  }
+  turnOn() {
+    this.isOn = true;
+  }
+  turnOff() {
+    this.isOn = false;
+    this.speed = 0;
+  }
+  accelerate(value) {
+    /*
+     * Этот метод должен добавлять
+     * к свойству `speed` полученное
+     * значение, при условии,
+     * что результирующая скорость
+     * не больше чем значение свойства `maxSpeed`
+     */
+    // accelerate(value) {}
+    if (this.speed + value < this.maxSpeed) {
+      return (this.speed = this.speed + value);
+    } else {
+      return (this.speed = this.maxSpeed);
+    }
+  }
+  decelerate(value) {
+    /*
+     * Этот метод должен отнимать
+     * от свойства `speed`
+     * полученное значение, при условии,
+     * что результирующая скорость не меньше 0
+     */
+    // decelerate(value) {}
+    if (this.speed - value > 0) {
+      return (this.speed = this.speed - value);
+    } else {
+      return (this.speed = 0);
+    }
+  }
+  drive(hours) {
+    /*
+     * Этот метод должен добавлять к свойству
+     * `distance` пробег в километрах,
+     * т.е. hours * speed,
+     * но только в том случае,
+     * если машина заведена!
+     */
+    // drive(hours) {}
+    if (this.isOn) {
+      return (this.distance += hours * this.speed);
+    } else {
+      return (this.distance = 0);
+    }
+  }
+}
 
-const invokeInventoryAction = function (itemName, action) {
-  const act = action(itemName);
-  const msg = `Invoking action on ${itemName}`;
-  return { act, msg };
-};
+const mustang = new Car({ maxSpeed: 200, price: 2000 });
+mustang.turnOn();
+mustang.accelerate(50);
+mustang.drive(2);
 
-const invokeAdd = invokeInventoryAction(
-  'Medkit',
-  inventory.add.bind(inventory), // Write code in this line ===было дописано .bind(inventory)===
-);
-const arrayAdd = [...inventory.items];
+console.log(Car.getSpecs(mustang));
+// 'maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000'
 
-console.log(invokeAdd);
-// { act: 'Adding Medkit to inventory', msg: 'Invoking action on Medkit' }
+mustang.decelerate(20);
+mustang.drive(1);
+mustang.turnOff();
 
-console.log(arrayAdd);
-// ['Knife', 'Gas mask', 'Medkit']
+console.log(Car.getSpecs(mustang));
+// 'maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000'
 
-const invokeRemove = invokeInventoryAction(
-  'Gas mask',
-  inventory.remove.bind(inventory), // Write code in this line ===было дописано .bind(inventory)===
-);
+console.log(mustang.price); // 2000
+mustang.price = 4000;
+console.log(mustang.price); // 4000
 
-const arrayRemove = [...inventory.items];
+// ------------------------------------------------------------------------------------------
+// ===== Задача 5-5 =====
+// ------ Напиши класс Car с указанными свойствами и методами. -----
+// class Car {
+/*
+ * Добавь `статический` метод
+ * `getSpecs(car)`, который принимает
+ * объект-машину как параметр
+ * и возвращает шаблонную строку
+ * со свойствами и значениями объекта.
+ * Свойства:
+ *   maxSpeed,
+ *   speed,
+ *   isOn,
+ *   distance,
+ *   price
+ * Пример строки, полученной этим методом:
+ * 'maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000'
+ */
 
-console.log(invokeRemove);
-//{ act: 'Removing Gas mask from inventory', msg: 'Invoking action on Gas mask' }
+/*
+ * Конструктор получает объект настроек.
+ *
+ * Добавь свойства будущеего экземпляра класса:
+ *  speed - текущая скорость,
+ *          начальное значение `0`
+ *  price - цена автомобиля
+ *  maxSpeed - максимальная скорость
+ *  isOn - заведен ли автомобиль.
+ *         Значения `true` или `false`,
+ *         начальное значение false
+ *  distance - пробег в километрах,
+ *             начальное значение `0`
+ */
+// constructor() {}
 
-console.log(arrayRemove);
-// ['Knife', 'Medkit']
+/*
+ * Добавь геттер и сеттер
+ * для свойства `price`, который будет
+ * работать с свойством цены автомобиля.
+ *
+ * ВАЖНО: для записи методов get и set
+ * значение параметра записывают с
+ * подчеркиванием. См. ниже пример 1.
+ */
 
-// -------------------------------------------------------------------------------------------------------------------------
-// ======================= Задача 4 - 5 ========================
-// ------------ bind для замены this в методах объекта ---------
-// Оформи вызов метода invokeInventoryAction таким образом, чтобы в качестве this для методов
+/*
+ * Метод, который заводит автомобиль
+ * Записывает в свойство `isOn` значение `true`
+ */
+// turnOn() {}
 
-// inventory.add
-// inventory.remove выступал объект inventory
+/*
+ * Метод, чтобы заглушить автомобиль
+ * Этот метод должен записывать
+ * в свойство isOn значение false,
+ * и сбрасывать текущую скорость до 0
+ */
+// turnOff() {}
+
+/*
+ * Этот метод должен добавлять
+ * к свойству `speed` полученное
+ * значение, при условии,
+ * что результирующая скорость
+ * не больше чем значение свойства `maxSpeed`
+ */
+// accelerate(value) {}
+
+/*
+ * Этот метод должен отнимать
+ * от свойства `speed`
+ * полученное значение, при условии,
+ * что результирующая скорость не меньше 0
+ */
+
+// decelerate(value) {}
+
+/*
+ * Этот метод должен добавлять к свойству
+ * `distance` пробег в километрах,
+ * т.е. hours * speed,
+ * но только в том случае,
+ * если машина заведена!
+ */
+// drive(hours) {}
+// }
+
+// #### Пример 1.
+
+// ```js
+// class Guest {
+// Собственные свойства класса размещаем в конструкторе
+// constructor(name, roomNumber) {
+/* параметр name не должен
+ *   совпадать с названием свойства
+ *   но должен быть похож.
+ *   Поэтому используют прием смены
+ *   названия добавляя символ '_'.
+ *   Хотя это может любой другой символ
+ */
+// this._name = name;
+// }
+
+// Используем геттеры и сеттеры для описания интерфейса доступа к свойствам
+// get name() {
+// return this._name;
+// }
+
+// set name(value) {
+// this._name = value;
+// }
+// }
